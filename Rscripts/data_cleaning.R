237564367ef7c1fae63ba5edfdf8bb210a299b61
GenericP3rson <genericp3rson@gmail.com>
2023-07-10 18:14:52 -0400

    added writing and merging

--- Rscripts/data_cleaning.R
+++ Rscripts/data_cleaning.R
@@ -1,6 +1,9 @@
-# separating row values into columns based on a separator
+## Separating row values into columns based on a separator
 
 library(tidyr)
 new_df <- as.data.frame(separate_rows(old_df, COLUMN_NAME, sep = "SEP"))
 
+## Merging two dfs based on a column
+
+new_df <- merge(df1, df2, by.x=DF1_COLNAME, by.y=DF2_COLNAME)
 
--- Rscripts/reading_writing_files.R
+++ Rscripts/reading_writing_files.R
@@ -2,13 +2,23 @@
 
 csv <- read.csv("/path/to/file.csv")
 
+## Writing CSVs
+
+write.csv(csv, "/path/to/file.csv")
+
 ## Reading TSVs (without libraries)
 
 tsv <- read.csv("/path/to/file.tsv", sep="\t")
+
+## Writing TSVs
+
+write.csv(tsv, "/path/to/file.tsv", sep="\t")
 
 ## Reading Excel Files (needs tidyverse's readxl file)
 
 library(readxl)
 
 excel <- as.data.frame(read_excel("/path/to/file.xlsx"))
+
+
 
