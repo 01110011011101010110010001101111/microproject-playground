aaf6d52015c2ab85988a273648bc3456a7f6cfd0
GenericP3rson <genericp3rson@gmail.com>
2023-07-10 14:03:00 -0400

    starting a commonly used R scripts folder/project

---
+++ Rscripts/README.md
@@ -0,0 +1,6 @@
+# Rscripts
+
+I've been doing a ton of data wrangling with R recently, so I thought I would upload some common techniques I find myself using (and having to search up again and again every time).
+
+If I actually build this up, think of this perhaps as a microcourse of R.
+
---
+++ Rscripts/reading_files.R
@@ -0,0 +1,14 @@
+## Reading CSVs (without libraries)
+
+csv <- read.csv("/path/to/file.csv")
+
+## Reading TSVs (without libraries)
+
+tsv <- read.csv("/path/to/file.tsv", sep="\t")
+
+## Reading Excel Files (needs tidyverse's readxl file)
+
+library(readxl)
+
+excel <- as.data.frame(read_excel("/path/to/file.xlsx"))
+
