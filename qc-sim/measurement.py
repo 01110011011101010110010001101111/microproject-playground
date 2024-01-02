ac20c90058832f3fbfcd6fb815adfe00c443dc70
GenericP3rson <genericp3rson@gmail.com>
2024-01-02 09:12:33 -0600

    added generalized measurement and expected value

--- qc-sim/measurement.py
+++ qc-sim/measurement.py
@@ -5,3 +5,17 @@ def raw_probabilities(quantum_state, basis_vec=None):
     if basis_vec is None:
         basis_vec = [1, 0]
     return (basis_vec.conjugate().dot(quantum_state))**2 
+
+def measure(quantum_state, basis):
+    """
+    Returns (probability, updated_state)
+    """
+    projected_quantum_state = basis @ quantum_state
+    prob = quantum_state.conjugate() @ projected_quantum_state
+    return prob, projected_quantum_state / prob**0.5
+
+def expected_value(quantum_state, observable):
+    """
+    computes probability given an observable
+    """
+    return quantum_state.conjugate() @ observable @ quantum_state
--- qc-sim/test.py
+++ qc-sim/test.py
@@ -5,16 +5,21 @@ one = np.array([0, 1])
 plus = 1/2**0.5 * (zero + one)
 minus = 1/2**0.5 * (zero - one)
 
+from measurement import *
 def lec_4_example():
     """
     Example from lec 4 notes: https://math.mit.edu/~shor/435-LN/Lecture_04.pdf
     """
-    from measurement import raw_probabilities
 
     phi = 4/5*zero + 3/5*one
 
     assert (np.isclose(raw_probabilities(phi, plus), 49/50, 0.01))
     assert (np.isclose(raw_probabilities(phi, minus), 1/50, 0.01))
+
+    pi_A = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+    pi_B = np.array([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+    state = np.array([5/10, 7/10, 5/10, 1/10])
+    print(measure(state, pi_A))
 
 if __name__ == "__main__":
     lec_4_example()
