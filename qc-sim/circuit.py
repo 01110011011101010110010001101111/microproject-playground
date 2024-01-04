1d28f7dfe4f47d8e31649590a9022cc288e23f9e
GenericP3rson <genericp3rson@gmail.com>
2024-01-04 10:28:31 -0600

    [wip] another circuit modification

--- qc-sim/circuit.py
+++ qc-sim/circuit.py
@@ -1,12 +1,9 @@
-class Gate:
-    def __init__(self, unitary, wire = None):
-        self.unitary = unitary
-        self.wire = wire
-
 class Circuit:
     def __init__(self, gates, n_wires):
         self.gates = gates
         self.n_wires = n_wires
+    def add_gate(self, unitary, wires):
+        unitary_dim = len(unitary)
     def forward(self):
         for gate in self.gates:
             pass
